1a61f4ec46c20de2bdf3a5a5f50f98f016f02122
gerome <geromeandrew7@gmail.com>
2025-03-27 11:41:40 +0800

    fixed deprecated imports

--- pinax/notifications/tests/urls.py
+++ pinax/notifications/tests/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path as url
 
 urlpatterns = [
     url(r"^notifications/", include("pinax.notifications.urls", namespace="pinax_notifications")),
